6cdd1e18a881d6404cc32f228420fb4de0c2c16b
Goose0066 <140986045+Goose0066@users.noreply.github.com>
2023-07-30 17:29:20 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,7 +1,7 @@
 /*   STEP 1   */
 export const leagueID = "916387629476802560"; // your league ID
 export const leagueName = "The League"; // your league name
-export const dues = 100; // (optional) used in template constitution page
+export const dues = 50; // (optional) used in template constitution page
 export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = true; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
